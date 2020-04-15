5cec9b07ba85692d35069fae1ac6d64ffbadf3df
8bitbran <brandonmar97@gmail.com>
2020-04-15 14:51:40 -0400

    Added new gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 
 require 'pry'
 require 'httparty'
+require 'colorize'
+require 'rmagick'
+require 'asciiart'
 
 require_relative '../lib/fortnite-tracker/api_manager'
 require_relative '../lib/fortnite-tracker/players'
