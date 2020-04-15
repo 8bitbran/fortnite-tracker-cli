0120c17ea6f2599250954ac49568a4d1d0a24284
8bitbran <brandonmar97@gmail.com>
2020-04-15 14:52:00 -0400

    Colorized titles and menus. Added ASCII art

--- lib/fortnite-tracker/cli.rb
+++ lib/fortnite-tracker/cli.rb
@@ -1,9 +1,11 @@
 class FortniteTracker::CLI
 
     attr_accessor :player
-    
+                                                                                                               
     def call 
-        puts "Welcome to Fortnite Tracker!\n\n"
+        fortnite_logo = AsciiArt.new("https://i.imgur.com/zwwIIFQ.jpg")
+        puts fortnite_logo.to_ascii_art                                                                                                        
+        puts "\n\nWelcome to Fortnite Tracker!\n\n".color(:red)
         puts "Track players' stats by platform and player name\n\n"
         start 
     end 
@@ -31,17 +33,17 @@ class FortniteTracker::CLI
     end 
 
     def menu 
-        puts "\nMenu Options:\n\n"
-        puts "Type 'all' for all stats"
-        puts "Type 'time played' for total time played"
-        puts "Type 'top fives' for total games with Top 5 placement"
-        puts "Type 'wins' for total wins"
-        puts "Type 'percentage' for win percentage"
-        puts "Type 'kills' for total kills"
-        puts "Type 'kdr' for kill-death ratio"
-        puts "Type 'history' for match history"
-        puts "Type 'player' to get a different player"
-        puts "\nType 'exit' to exit\n\n"
+        puts "\nMenu Options:\n\n".color(:red)
+        puts "Type #{"'all'".color(:yellow)} for all stats"
+        puts "Type #{"'time played'".color(:yellow)} for total time played"
+        puts "Type #{"'top fives'".color(:yellow)} for total games with Top 5 placement"
+        puts "Type #{"'wins'".color(:yellow)} for total wins"
+        puts "Type #{"'percentage'".color(:yellow)} for win percentage"
+        puts "Type #{"'kills'".color(:yellow)} for total kills"
+        puts "Type #{"'kdr'".color(:yellow)} for kill-death ratio"
+        puts "Type #{"'history'".color(:yellow)} for match history"
+        puts "Type #{"'change player'".color(:yellow)} to get a different player"
+        puts "\nType #{"'exit'".color(:red)} to exit\n\n"
     end
 
     def track_player
@@ -51,36 +53,36 @@ class FortniteTracker::CLI
             input = gets.chomp.downcase
             case input
                 when "all"
-                    puts "\nAll Stats for #{player.name}\n\n"
+                    puts "\nAll Stats for #{player.name.color(:red)}\n\n"
                     player.all_stats
                     puts "\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)} for menu: \n\n"
                 when "time played"
-                    puts "\n#{player.name} has played #{player.time_played}\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has played #{player.time_played.color(:yellow)}\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "top fives"
-                    puts "\n#{player.name} has placed top 5 in #{player.top_fives} games\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has placed top 5 in #{player.top_fives.color(:yellow)} games\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "wins"
-                    puts "\n#{player.name} has won #{player.wins} games\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has won #{player.wins.color(:yellow)} games\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "percentage"
-                    puts "\n#{player.name} has a win percentage of #{player.win_percentage}\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has a win percentage of #{player.win_percentage.color(:yellow)}\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "kills"
-                    puts "\n#{player.name} has #{player.kills} total kills\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has #{player.kills.color(:yellow)} total kills\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "kdr"
-                    puts "\n#{player.name} has a KDR of #{player.kdr}\n\n"
-                    puts "Enter another option or type 'menu' for menu: \n\n"
+                    puts "\n#{player.name.color(:red)} has a KDR of #{player.kdr.color(:yellow)}\n\n"
+                    puts "Enter another option or type #{"'menu'".color(:red)}  for menu: \n\n"
                 when "history"
-                    puts "\n"
+                    puts "\n#{"Match History".color(:yellow)} for #{player.name.color(:red)}\n\n"
                     puts "#{player.match_history}"
                     puts "\n"
-                    puts "Enter another option or 'menu' for menu: \n\n"
+                    puts "Enter another option or #{"'menu'".color(:red)}  for menu: \n\n"
                 when "menu"
                     menu 
-                when "player"
+                when "change player"
                     puts "\n"
                     start
                 when "exit"
