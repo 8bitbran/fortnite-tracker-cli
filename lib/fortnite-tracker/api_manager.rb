16d808034089ed082ea982eda809c920e6b7a08a
8bitbran <brandonmar97@gmail.com>
2020-04-16 17:19:38 -0400

    Fixed bad URI requests

--- lib/fortnite-tracker/api_manager.rb
+++ lib/fortnite-tracker/api_manager.rb
@@ -4,6 +4,7 @@ class FortniteTracker::APIManager
 
     def self.get_player(platform, player)
         headers = {"TRN-Api-Key" => "5b6dbd29-9779-4709-85cf-ce5174b36f90"}
+        player = player.gsub(" ", "%20")
         response = HTTParty.get("#{BASE_URL}#{platform}/#{player}", headers: headers)
         if response.include?("\n")
             puts "\nThe username is invalid, please re-enter your info\n\n"
@@ -13,7 +14,6 @@ class FortniteTracker::APIManager
             FortniteTracker::CLI.new.start
         else 
             FortniteTracker::Players.create(response)
-            
         end
     end
 end 
